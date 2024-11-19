0c6941d3776e90be363ddd2e7a54bc7038414ae8
untot1 <luispb98@hotmail.com>
2024-11-19 15:07:24 -0600

    fix routes

--- server.js
+++ server.js
@@ -4,24 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the "views" directory
+app.use(express.static(path.join(__dirname, 'views')));
+
 // Serve static files from the "images" directory
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// Serve the HTML files from the "views" directory
+// Default route for the home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'home.html'));
-});
-
-app.get('/recetas', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'recetas.html'));
-});
-
-app.get('/subs', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'subs.html'));
-});
-
-app.get('/contacto', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'contacto.html'));
 });
 
 // Start the server
