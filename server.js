caf0d9b50ec1af643686b7cbd75d1ec23c4a58e8
untot1 <luispb98@hotmail.com>
2024-11-19 15:10:20 -0600

    fix routes

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 // Default route for the home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'home.html'));
+  app.get('/recetas', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'recetas.html'));
+  });
+  
+  app.get('/subs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'subs.html'));
+  });
+  
+  app.get('/contacto', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'contacto.html'));
+  });
 });
 
 // Start the server
